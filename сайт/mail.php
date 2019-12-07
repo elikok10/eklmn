dd0a4f0ca47c273ed4c8d3ef996eb0c5369df852
Ilya <S-Mettem@users.noreply.github.com>
2019-12-08 02:48:08 +0700

    Сделан php

--- сайт/mail.php
+++ сайт/mail.php
@@ -4,22 +4,23 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
+$email = $_POST['user-mail'];
+$name = $_POST['username'];
+$phone = $_POST['user-tel'];
+$user_req = $_POST['user-request'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'ssl://smtp.yandex.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'William53ify@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Dota2Warcraft3'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('William53ify@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('William53ify@yandex.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -28,8 +29,8 @@ $mail->addAddress('gomudusu@p33.org');     // Кому будет уходить
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Subject = 'Новая заявка!!!';
+$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '. Почта этого пользователя: ' .$email. '<br>Его проблема: <br>' .$user_req;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
